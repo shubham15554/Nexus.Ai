84e6026e8a19b58e8361790c450233d45196c9b3
Shubham Yadav <ry957933@gmail.com>
2026-06-08 22:11:35 +0530

    case

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,13 +1,13 @@
 
 const router = require("express").Router();
 const User = require("../models/user");
-const authmiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 const {createChat , deleteChat , getChats, getMessages} = require("../controllers/chatController");
 
-router.get("/message/:id" , authmiddleware , getMessages);
-router.get("/" , authmiddleware , getChats);
-router.post("/create" , authmiddleware, createChat);
+router.get("/message/:id" , authMiddleware , getMessages);
+router.get("/" , authMiddleware , getChats);
+router.post("/create" , authMiddleware, createChat);
 
-router.post("/delete" ,authmiddleware, deleteChat);
+router.post("/delete" ,authMiddleware, deleteChat);
 
 module.exports = router;
